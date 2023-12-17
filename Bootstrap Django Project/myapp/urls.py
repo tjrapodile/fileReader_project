4c92d4045dbea4c7f7364c4b03b2a1c0bd68677a
tjrapodile <47654804+tjrapodile@users.noreply.github.com>
2023-12-17 13:47:13 +0200

    Add files via upload

---
+++ Bootstrap Django Project/myapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import upload_csv,upload_success,arima_models_view,index
+
+urlpatterns = [
+    path('index/', index, name='index'),
+    path('upload/', upload_csv, name='upload_csv'),
+    path('upload_success/', upload_success, name='upload_success'),
+    path('arima_models_view.html', arima_models_view, name='arima_models_view'),
+]
+
+
+
